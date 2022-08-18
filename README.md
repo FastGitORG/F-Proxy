3825841528772b82e0c74985e5630e05d605fa59
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2022-08-18 23:55:21 +0100

    feat: base

---
+++ README.md
@@ -0,0 +1,4 @@
+# F-Proxy
+
+F-Proxy is a simple solution for forward proxying over CloudFlare Wrap.
+
---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+ensureRoot
+setupRepository
+installWarp
+setupWarp
+startWarpProxy
+
+ensureRoot() {
+    if [[ $(id -u) != 0 ]]; then
+        log ERROR "Root is required."
+        exit 1
+    fi
+}
+
+setupRepository() {
+    curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
+    apt-get update
+}
+
+installWarp() {
+    apt-get install cloudflare-warp -y
+}
+
+setupWarp() {
+    warp-cli --accept-tos register
+}
+
+startWarpProxy() {
+    # default proxy is :40000
+    echo "[WARP] Set Mode"
+    warp-cli --accept-tos set-mode proxy
+    echo "[WARP] CONNECT"
+    warp-cli --accept-tos connect
+    echo "[WARP] ENABLE ALWAYS ON"
+    warp-cli --accept-tos enable-always-on
+}
+
+installGoSniProxy() {
+    #/usr/local/bin
+    #wget -O /usr/local/bin/sniproxy
+    #chmod +x /usr/local/bin/sniproxy
+}
+
+runGoSniProxy() {
+
+}
